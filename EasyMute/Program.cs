88705683c7f90b0e5b5e51fdc617e11f5d4fe96e
Denis Block <denis_b555@live.com>
2022-04-09 04:34:00 +0200

    Allow only single instance of app running simultaneously

--- EasyMute/Program.cs
+++ EasyMute/Program.cs
@@ -9,15 +9,26 @@ namespace EasyMute
 {
     internal static class Program
     {
+        static readonly Mutex _Mutex = new Mutex(true, "EasyMuteMutex");
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AppContext());
+            if (!_Mutex.WaitOne(TimeSpan.Zero, false))
+            {
+                return;
+            }
+
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new AppContext());
+            }
+            finally { _Mutex.ReleaseMutex(); }
         }
     }
 }
